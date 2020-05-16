c5fdd6dbdb469a70521c415e4bbceb06e785d33d
Vulcan <vulcan.lee@gmail.com>
2020-05-16 16:46:17 +0800

    對話窗元件，使用 Inline 與 Code Behind 兩種做法

---
+++ SyncfusionLab/SyncfusionLab/Components/Commons/ConfirmBox.razor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyncfusionLab.Components.Commons
+{
+    using Microsoft.AspNetCore.Components;
+    public class ConfirmBoxBase : ComponentBase
+    {
+        [Parameter]
+        public bool IsVisible { get; set; } = false;
+        [Parameter]
+        public string Height { get; set; } = "250px";
+        [Parameter]
+        public string Width { get; set; } = "435px";
+        [Parameter]
+        public string Title { get; set; } = "通知訊息";
+        [Parameter]
+        public string Message { get; set; } = "訊息內容";
+        [Parameter]
+        public EventCallback<bool> Callback { get; set; }
+        public async Task OnOKBtnclick()
+        {
+            await CallCallBack(true);
+        }
+        public async Task OnCancelBtnclick()
+        {
+            await CallCallBack(false);
+        }
+
+        public async Task CallCallBack(bool action)
+        {
+            if (Callback.HasDelegate)
+                await Callback.InvokeAsync(action);
+        }
+    }
+}

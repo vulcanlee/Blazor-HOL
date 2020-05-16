a52cd313ab9de329315c9cde37efc1aec2534a2c
Vulcan <vulcan.lee@gmail.com>
2020-05-16 17:39:26 +0800

    新增 PersonService

---
+++ SyncfusionLab/SyncfusionLab/Services/PersonService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyncfusionLab.Services
+{
+    using EFCoreModel.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class PersonService
+    {
+        private readonly SchoolContext context;
+
+        public PersonService(SchoolContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<IQueryable<Person>> GetAsync()
+        {
+            return Task.FromResult(context.Person
+                .AsNoTracking().AsQueryable());
+        }
+
+        public async Task<Person> GetAsync(int id)
+        {
+            Person item = await context.Person
+                .FirstOrDefaultAsync(x => x.PersonId == id);
+            return item;
+        }
+
+        public async Task<bool> AddAsync(Person paraObject)
+        {
+            await context.Person.AddAsync(paraObject);
+            await context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> UpdateAsync(Person paraObject)
+        {
+            Person item = await context.Person
+                .FirstOrDefaultAsync(x => x.PersonId == paraObject.PersonId);
+            if (item == null)
+            {
+                return false;
+            }
+            else
+            {
+                #region 在這裡需要設定需要更新的紀錄欄位值
+                context.Entry(item).State = EntityState.Modified;
+                #endregion
+                await context.SaveChangesAsync();
+                return true;
+            }
+
+        }
+
+        public async Task<bool> DeleteAsync(Person paraObject)
+        {
+            Person item = await context.Person
+                .FirstOrDefaultAsync(x => x.PersonId == paraObject.PersonId);
+            if (item == null)
+            {
+                return false;
+            }
+            else
+            {
+                context.Person.Remove(item);
+                await context.SaveChangesAsync();
+                return true;
+            }
+        }
+    }
+}

81334c7566a6e1638227a5cec250640b5f1f37ad
Vulcan <vulcan.lee@gmail.com>
2020-05-17 22:50:50 +0800

    建立 MatBlazor UI Library 的練習專案

---
+++ MatBlazorLab/EFCoreModel/Models/OnlineCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreModel.Models
+{
+    public partial class OnlineCourse
+    {
+        public int OnlineCourseId { get; set; }
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+
+        public virtual Course Course { get; set; }
+    }
+}

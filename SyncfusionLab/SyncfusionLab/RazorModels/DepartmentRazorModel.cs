ef1ae4c5b862979e1980f1d355b163404632afc1
Vulcan <vulcan.lee@gmail.com>
2020-05-17 18:31:14 +0800

    建立 Department 頁面要用到的 Razor 資料模型 Razor Data Model

---
+++ SyncfusionLab/SyncfusionLab/RazorModels/DepartmentRazorModel.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyncfusionLab.RazorModels
+{
+    using AutoMapper;
+    using EFCoreModel.Models;
+    using SyncfusionLab.AdaptorModels;
+    using SyncfusionLab.Interfaces;
+    using SyncfusionLab.Services;
+    using Syncfusion.Blazor.Grids;
+    using Microsoft.AspNetCore.Components.Forms;
+
+    public class DepartmentRazorModel
+    {
+        #region Constructor
+        public DepartmentRazorModel(IDepartmentService CurrentService,
+           SchoolContext SchoolContext,
+           IMapper Mapper)
+        {
+            this.CurrentService = CurrentService;
+            this.SchoolContext = SchoolContext;
+            mapper = Mapper;
+        }
+        #endregion
+
+        #region Property
+        public SfGrid<DepartmentAdaptorModel> Grid { get; set; }
+        public bool IsShowEditRecord { get; set; } = false;
+        public DepartmentAdaptorModel CurrentRecord { get; set; } = new DepartmentAdaptorModel();
+        public DepartmentAdaptorModel CurrentNeedDeleteRecord { get; set; } = new DepartmentAdaptorModel();
+        public EditContext LocalEditContext { get; set; }
+
+
+        #region 訊息說明之對話窗使用的變數
+        public ConfirmBoxModel ConfirmMessageBox { get; set; } = new ConfirmBoxModel();
+        public MessageBoxModel MessageBox { get; set; } = new MessageBoxModel();
+        #endregion
+
+        public string EditRecordDialogTitle { get; set; } = "";
+        #endregion
+
+        #region Field
+        public bool ShowAontherRecordPicker { get; set; } = false;
+        bool isNewRecordMode;
+        private readonly IDepartmentService CurrentService;
+        private readonly SchoolContext SchoolContext;
+        private readonly IMapper mapper;
+        IRazorPage thisRazorComponent;
+        private bool isShowConfirm { get; set; } = false;
+        #endregion
+
+        #region Method
+        public void Setup(IRazorPage componentBase,
+            SfGrid<DepartmentAdaptorModel> grid)
+        {
+            thisRazorComponent = componentBase;
+            Grid = grid;
+            //RazorModel
+        }
+        public void OnOpenPicker()
+        {
+            ShowAontherRecordPicker = true;
+        }
+
+        public void ToolbarClickHandler(Syncfusion.Blazor.Navigations.ClickEventArgs args)
+        {
+            if (args.Item.Text == "新增")
+            {
+                CurrentRecord = new DepartmentAdaptorModel();
+                EditRecordDialogTitle = "新增紀錄";
+                isNewRecordMode = true;
+                IsShowEditRecord = true;
+            }
+        }
+        public void OnCommandClicked(CommandClickEventArgs<DepartmentAdaptorModel> args)
+        {
+            DepartmentAdaptorModel item = args.RowData as DepartmentAdaptorModel;
+            if (args.CommandColumn.ButtonOption.Content == "修改")
+            {
+                CurrentRecord = item;
+                EditRecordDialogTitle = "修改紀錄";
+                IsShowEditRecord = true;
+                isNewRecordMode = false;
+
+            }
+            else if (args.CommandColumn.ButtonOption.Content == "刪除")
+            {
+                #region 檢查關聯資料是否存在
+                #endregion
+                CurrentNeedDeleteRecord = item;
+                ConfirmMessageBox.Show("400px", "200px", "警告", "確認要刪除這筆紀錄嗎？");
+            }
+        }
+
+        public async Task RemoveThisRecord(bool NeedDelete)
+        {
+            if (NeedDelete == true)
+            {
+                await CurrentService.DeleteAsync(mapper.Map<Department>(CurrentNeedDeleteRecord));
+                Grid.Refresh();
+            }
+            ConfirmMessageBox.Hidden();
+        }
+
+        public void OnCancel()
+        {
+            IsShowEditRecord = false;
+        }
+
+        public async Task HandleValidSubmit()
+        {
+            #region 進行 Form Validation 檢查驗證作業
+            if (LocalEditContext.Validate() == false)
+            {
+                return;
+            }
+            #endregion
+
+            #region 檢查資料完整性
+            #endregion
+
+            if (IsShowEditRecord == true)
+            {
+                if (isNewRecordMode == true)
+                {
+                    await CurrentService.AddAsync(mapper.Map<Department>(CurrentRecord));
+                    Grid.Refresh();
+                }
+                else
+                {
+                    await CurrentService.UpdateAsync(mapper.Map<Department>(CurrentRecord));
+                    Grid.Refresh();
+                }
+                IsShowEditRecord = false;
+            }
+        }
+        public void OnEditContestChanged(EditContext context)
+        {
+            LocalEditContext = context;
+        }
+
+        public void SortTest()
+        {
+            Grid.SortSettings.Columns = new List<GridSortColumn>();
+            Grid.SortSettings.Columns.Add(new GridSortColumn()
+            {
+                Field = "LastName",
+                Direction = SortDirection.Ascending
+            });
+            Grid.Refresh();
+        }
+        #endregion
+    }
+}

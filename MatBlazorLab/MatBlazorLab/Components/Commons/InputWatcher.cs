4fd48cdc9969ce1dc6d47345e37d78f1d6b814a5
Vulcan <vulcan.lee@gmail.com>
2020-05-20 17:42:16 +0800

    建立與測試 MatBlazor 元件可以使用

---
+++ MatBlazorLab/MatBlazorLab/Components/Commons/InputWatcher.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatBlazorLab.Components.Commons
+{
+    public class InputWatcher : ComponentBase
+    {
+        private EditContext editContext;
+
+        [CascadingParameter]
+        protected EditContext EditContext
+        {
+            get => editContext;
+            set
+            {
+                editContext = value;
+                EditContextActionChanged?.Invoke(editContext);
+            }
+        }
+
+        [Parameter]
+        public Action<EditContext> EditContextActionChanged { get; set; }
+    }
+}

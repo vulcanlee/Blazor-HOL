e95cc47bddcbb290a6557e36d672266b6f7226ce
Vulcan <vulcan.lee@gmail.com>
2020-05-25 17:22:13 +0800

    Console 專案與 EF Core 讀取已經存在的資料庫

---
+++ Labs/DBEntityFrameworkCore/DBEntityFrameworkCore/Program.cs
@@ -0,0 +1,24 @@
+﻿using DBEntityFrameworkCore.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBEntityFrameworkCore
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            SchoolContext context = new SchoolContext();
+            var people = await context.Person
+                .OrderBy(x=>x.LastName)
+                .ThenBy(x=>x.FirstName)
+                .ToListAsync();
+            foreach (var item in people)
+            {
+                Console.WriteLine($"人員:{item.LastName} {item.FirstName}");
+            }
+        }
+    }
+}

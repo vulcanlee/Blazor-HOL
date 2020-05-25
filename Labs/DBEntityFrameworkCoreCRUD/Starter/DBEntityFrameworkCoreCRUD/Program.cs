9d51bf48d1395cf5651dd0d2903c80ab28ec0c68
Vulcan <vulcan.lee@gmail.com>
2020-05-26 00:07:54 +0800

    用Console 專案對 person 資料表做 CRUD 存取 DBEntityFrameworkCoreCRUD\Starter

---
+++ Labs/DBEntityFrameworkCoreCRUD/Starter/DBEntityFrameworkCoreCRUD/Program.cs
@@ -0,0 +1,13 @@
+﻿using DBEntityFrameworkCoreCRUD.Model;
+using System;
+
+namespace DBEntityFrameworkCoreCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolContext context = new SchoolContext();
+        }
+    }
+}

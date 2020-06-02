3f89a4a34df2d1adf26604edc2230380dfc7a524
Vulcan <vulcan.lee@gmail.com>
2020-06-02 10:22:05 +0800

    加入檔案支援處理方法與相關資料夾

---
+++ xfLab/xfLab/xfLab/Helpers/Storages/StorageJSONService.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xfLab.Helpers.Storages
+{
+    public class StorageJSONService<T>
+    {
+
+        /// <summary>
+        /// Loads data from a file
+        /// </summary>
+        /// <param name="fileName">Name of the file to read.</param>
+        /// <returns>Data object</returns>
+        public static async Task<T> ReadFromFileAsync(string directoryName, string fileName, bool 需要加解密 = false)
+        {
+            //T loadedFile = default(T);
+            T loadedFile = (T)Activator.CreateInstance(typeof(T));
+            string tempStr = "";
+            try
+            {
+                tempStr = await StorageUtility.ReadFromDataFileAsync("", directoryName, fileName, 需要加解密);
+                loadedFile = JsonConvert.DeserializeObject<T>(tempStr);
+            }
+            catch
+            {
+                //ApplicationState.ErrorLog.Add(new ErrorLog("LoadFromFile", e.Message));
+            }
+
+            return loadedFile;
+        }
+
+        public static T LoadFromString(string SourceString)
+        {
+            T loadedFile = (T)Activator.CreateInstance(typeof(T));
+            try
+            {
+                loadedFile = JsonConvert.DeserializeObject<T>(SourceString);
+            }
+            catch
+            {
+                //ApplicationState.ErrorLog.Add(new ErrorLog("LoadFromFile", e.Message));
+            }
+
+            return loadedFile;
+        }
+
+        /// <summary>
+        /// Saves data to a file.
+        /// </summary>
+        /// <param name="fileName">Name of the file to write to</param>
+        /// <param name="data">The data to save</param>
+        public static async Task WriteToDataFileAsync(string directoryName, string fileName, T data, bool 需要加解密 = false)
+        {
+            try
+            {
+                string output = JsonConvert.SerializeObject(data);
+                await StorageUtility.WriteToDataFileAsync("", directoryName, fileName, output, 需要加解密);
+            }
+            catch
+            {
+                // Add desired error handling for your application
+                // ApplicationState.ErrorLog.Add(new ErrorLog("SaveToFile", e.Message));
+            }
+        }
+
+    }
+}
---
+++ xfLab/xfLab/xfLab/Helpers/Storages/StorageUtility.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace xfLab.Helpers.Storages
+{
+    /// <summary>
+    /// Storage 相關的 API
+    /// http://www.nudoq.org/#!/Packages/PCLStorage/PCLStorage/FileSystem
+    /// </summary>
+    public class StorageUtility
+    {
+        /// <summary>
+        /// 存取 Resource URI
+        /// </summary>
+        public static readonly string BaseUri = "ms-appx:///";
+        /// <summary>
+        /// 存取 Local Data URI
+        /// </summary>
+        public static readonly string LocalUri = "ms-appdata:///local/";
+
+        /// <summary>
+        /// 將所指定的字串寫入到指定目錄的檔案內
+        /// </summary>
+        /// <param name="folderName">目錄名稱</param>
+        /// <param name="filename">檔案名稱</param>
+        /// <param name="content">所要寫入的文字內容</param> 
+        /// <returns></returns>
+        public static async Task WriteToDataFileAsync(string folderPath, string folderName, string filename, string content, bool 需要加解密 = true)
+        {
+            string rootFolder = FileSystem.AppDataDirectory;
+
+            if (string.IsNullOrEmpty(folderName))
+            {
+                throw new ArgumentNullException(nameof(folderName));
+            }
+
+            if (string.IsNullOrEmpty(filename))
+            {
+                throw new ArgumentNullException(nameof(filename));
+            }
+
+            if (string.IsNullOrEmpty(content))
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            #region 建立與取得指定路徑內的資料夾
+            try
+            {
+                string[] fooPaths = folderName.Split(new string[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+                string tempRootFolder = rootFolder;
+                foreach (var item in fooPaths)
+                {
+                    tempRootFolder = Path.Combine(tempRootFolder, item);
+                    var info = Directory.CreateDirectory(tempRootFolder);
+                }
+
+                tempRootFolder = Path.Combine(tempRootFolder, filename);
+                await Task.Run(() =>
+                {
+                    File.WriteAllText(tempRootFolder, content);
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+            #endregion
+        }
+
+        /// <summary>
+        /// 從指定目錄的檔案內將文字內容讀出
+        /// </summary>
+        /// <param name="folderName">目錄名稱</param>
+        /// <param name="filename">檔案名稱</param>
+        /// <returns>文字內容</returns>
+        public static async Task<string> ReadFromDataFileAsync(string folderPath, string folderName, string filename, bool 需要加解密 = true)
+        {
+            string content = "";
+
+            string rootFolder = FileSystem.AppDataDirectory;
+
+            if (string.IsNullOrEmpty(folderName))
+            {
+                throw new ArgumentNullException(nameof(folderName));
+            }
+
+            if (string.IsNullOrEmpty(filename))
+            {
+                throw new ArgumentNullException(nameof(filename));
+            }
+
+            #region 建立與取得指定路徑內的資料夾
+            try
+            {
+                string[] fooPaths = folderName.Split(new string[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+                string tempRootFolder = rootFolder;
+                foreach (var item in fooPaths)
+                {
+                    tempRootFolder = Path.Combine(tempRootFolder, item);
+                    var info = Directory.CreateDirectory(tempRootFolder);
+                }
+
+                tempRootFolder = Path.Combine(tempRootFolder, filename);
+                if(File.Exists(tempRootFolder))
+                {
+                    await Task.Run(() =>
+                    {
+                        content = File.ReadAllText(tempRootFolder);
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+            #endregion
+
+            return content.Trim();
+        }
+    }
+}

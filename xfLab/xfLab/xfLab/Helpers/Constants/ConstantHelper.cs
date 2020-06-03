f78d664040fbe9310b241f6f06fed9522efa6bf8
Vulcan <vulcan.lee@gmail.com>
2020-06-03 10:18:59 +0800

    HttpClient 的 Log 支援方法

---
+++ xfLab/xfLab/xfLab/Helpers/Constants/ConstantHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xfLab.Helpers.Constants
+{
+    class ConstantHelper
+    {
+        public static string HTTPRequestPayload { get; set; } = "";
+    }
+}
---
+++ xfLab/xfLab/xfLab/Helpers/WebAPIs/LoggingHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using xfLab.Helpers.Constants;
+
+namespace xfLab.Helpers.WebAPIs
+{
+    public class LoggingHandler : DelegatingHandler
+    {
+        public LoggingHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            ConstantHelper.HTTPRequestPayload = "";
+            ConstantHelper.HTTPRequestPayload = $"Request : {request.ToString()}{Environment.NewLine}{Environment.NewLine}";
+            Debug.WriteLine(new String('-', 40));
+            Debug.WriteLine("      " + "Request:");
+            Debug.WriteLine("              " + request.ToString());
+            if (request.Content != null)
+            {
+                Debug.WriteLine("              " + await request.Content.ReadAsStringAsync());
+                ConstantHelper.HTTPRequestPayload += $"{await request.Content.ReadAsStringAsync()}{Environment.NewLine}";
+            }
+            Debug.WriteLine(new String('-', 40));
+            Debug.WriteLine("");
+            ConstantHelper.HTTPRequestPayload += $"{Environment.NewLine}{Environment.NewLine}";
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            ConstantHelper.HTTPRequestPayload += $"Response : {request.ToString()}";
+
+            Debug.WriteLine(new String('-', 40));
+            Debug.WriteLine("      " + "Response:");
+            Debug.WriteLine("              " + response.ToString());
+            if (response.Content != null)
+            {
+                Debug.WriteLine("              " + await response.Content.ReadAsStringAsync());
+                ConstantHelper.HTTPRequestPayload += $"{await response.Content.ReadAsStringAsync()}{Environment.NewLine}";
+            }
+            Debug.WriteLine(new String('-', 40));
+            Debug.WriteLine("");
+            ConstantHelper.HTTPRequestPayload += $"{Environment.NewLine}{Environment.NewLine}";
+
+            return response;
+        }
+    }
+}

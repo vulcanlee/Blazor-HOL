1ac409c360724fb772d4a8ac732ddb00b28b85c1
Vulcan <vulcan.lee@gmail.com>
2020-08-23 17:16:50 +0800

    單一 Department 資料表的 CRUD 開發過程 建立 DepartmentAdapter，用來提供 sfGrid 元件要用的資料庫紀錄來源

--- StepByStep/07SingleCRUD/School.WebApp/Helpers/AutoMapping.cs
+++ StepByStep/07SingleCRUD/School.WebApp/Helpers/AutoMapping.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 namespace School.WebApp.Helpers
 {
     using AutoMapper;
+    using EFCore.Models;
+    using School.WebApp.AdapterModels;
 
     public class AutoMapping : Profile
     {
         public AutoMapping()
         {
+            CreateMap<Department, DepartmentAdapterModel>();
+            CreateMap<DepartmentAdapterModel, Department>();
         }
     }
 }

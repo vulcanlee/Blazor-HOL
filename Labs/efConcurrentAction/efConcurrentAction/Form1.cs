21f472034c66922cddaf9da903649f3ed91ed98b
Vulcan <vulcan.lee@gmail.com>
2020-09-04 16:43:41 +0800

    請觀察專案 - 同時多次進行異動，會發生甚麼問題

---
+++ Labs/efConcurrentAction/efConcurrentAction/Form1.cs
@@ -0,0 +1,54 @@
+﻿using efConcurrentAction.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace efConcurrentAction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 30 * 1000;
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            label1.Text = "定時器已經啟動 "+DateTime.Now.AddSeconds(30).ToString();
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            using (var context = new SchoolContext() )
+            {
+                await BeginProcessing(context);
+            }
+        }
+
+        private async Task BeginProcessing(SchoolContext context)
+        {
+            var StudentGrades = await context.StudentGrade
+                .AsNoTracking()
+                .ToListAsync();
+            foreach (var item in StudentGrades)
+            {
+                var currentThread = Thread.CurrentThread.ManagedThreadId;
+                Debug.WriteLine($"當前執行緒 : {currentThread}");
+                context.Entry(item).State = EntityState.Deleted;
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Labs/efConcurrentAction/efConcurrentAction/Models/Outline.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace efConcurrentAction.Models
+{
+    public partial class Outline
+    {
+        public int OutlineId { get; set; }
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+
+        public virtual Course Course { get; set; }
+    }
+}

cc30bc182dc9ed7f49dde6befd60fa3526752ed0
Vulcan <vulcan.lee@gmail.com>
2020-09-05 15:36:48 +0800

    使用 LoggerFactory 觀察送出的 SQL Statement

---
+++ Labs/efLoggerFactory/efLoggerFactory/Program.cs
@@ -0,0 +1,33 @@
+﻿using efLoggerFactory.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace efLoggerFactory
+{
+    class Program
+    {
+        public static readonly ILoggerFactory MyLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+            });
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=School";
+            DbContextOptions<SchoolContext> options = new DbContextOptionsBuilder<SchoolContext>()
+                .UseLoggerFactory(MyLoggerFactory)
+                .UseSqlServer(connectionString)
+                .Options;
+            using (var context = new SchoolContext(options))
+            {
+                Console.WriteLine($"取得 StudentGrade 第一筆紀錄");
+                var aStudentGrade = context.StudentGrade.FirstOrDefault();
+                Console.WriteLine($"更新成績為 4.99");
+                aStudentGrade.Grade = 4.99m;
+                context.SaveChanges();
+            }
+        }
+    }
+}

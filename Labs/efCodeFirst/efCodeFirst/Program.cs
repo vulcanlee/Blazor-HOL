1c477deddea7f853aed515ce66afc7b72753eda3
Vulcan <vulcan.lee@gmail.com>
2020-09-05 15:02:47 +0800

    使用 Code First 建立 Entity Framework Core 應用專案

---
+++ Labs/efCodeFirst/efCodeFirst/Program.cs
@@ -0,0 +1,15 @@
+﻿using efCodeFirst.Models;
+using System;
+
+namespace efCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DataContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}

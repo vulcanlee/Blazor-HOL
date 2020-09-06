cb8982788c044c97cecd7eb4dd75463d4e87d0b7
Vulcan <vulcan.lee@gmail.com>
2020-09-06 12:32:45 +0800

    EF Core 的 紀錄新增

---
+++ Labs/efAddRecord/efCodeFirst/Models/StudentAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace efCodeFirst.Models
+{
+    public class StudentAddress
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+        public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ Labs/efAddRecord/efCodeFirst/Program.cs
@@ -0,0 +1,34 @@
+﻿using efCodeFirst.Models;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Linq;
+
+namespace efCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DataContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            Department department1 = new Department()
+            {
+                Name = "新增的科系1",
+            };
+            context.Department.Add(department1);
+            context.SaveChanges();
+            Department department2 = new Department()
+            {
+                Name = "新增的科系2",
+            };
+            context.Entry(department2).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            context.SaveChanges();
+            foreach (var item in context.Department.ToList())
+            {
+                Console.WriteLine($"科系名稱:{item.Name}");
+            }
+        }
+    }
+}

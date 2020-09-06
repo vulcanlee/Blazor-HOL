bed2ebb37b56ed568ad3490b0544321f9ac12a91
Vulcan <vulcan.lee@gmail.com>
2020-09-06 13:31:31 +0800

    EF Core 的 紀錄刪除

---
+++ Labs/efDeleteRecord/efCodeFirst/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace efCodeFirst.Models
+{
+    public class Department
+    {
+        public Department()
+        {
+            Courses = new HashSet<Course>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
---
+++ Labs/efDeleteRecord/efCodeFirst/Program.cs
@@ -0,0 +1,40 @@
+﻿using efCodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Linq;
+
+namespace efCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DataContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            context.Department.Add(new Department());
+            context.Entry(new Department()).State = EntityState.Added;
+            context.SaveChanges();
+            Console.WriteLine($"科系記錄數量:{context.Department.ToList().Count}");
+            var departments = context.Department.ToList();
+            Reset(context); // 清除 ChangeTracker 內的科系紀錄
+            context.Department.Remove(departments[0]);
+            context.Entry(departments[1]).State = EntityState.Deleted;
+            context.SaveChanges();
+            Console.WriteLine($"科系記錄數量:{context.Department.ToList().Count}");
+        }
+        public static void Reset(DataContext context)
+        {
+            #region 解除快取紀錄
+            foreach (var item in context
+                .Set<Department>().Local.ToList())
+            {
+                context.Entry(item).State = 
+                    Microsoft.EntityFrameworkCore.EntityState.Detached;
+            }
+            #endregion
+        }
+    }
+}
